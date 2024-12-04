370af94c8ea57406efa4a1fa35a1776146164036
Estevan Sousa <estevanmartins03@gmail.com>
2024-12-04 14:42:08 -0300

    feat: add app.js to call Express

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-import http from "http";
+// Não precisamos mais disso...
+// import http from "http";
+// Mas precisamos do nosso app.js !
+import app from "./src/app.js";
+// Definindo a porta:
 const PORT = 3000;
-const server = http.createServer(
-    (req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain" } );
-    res.end("Hello World!");
-    }
-);
-
-server.listen( PORT,
+// Não precisaremos desse objeto rotas...
+/* const rotas = {
+"/": "Hello World!",
+"/livros": "Entrei na rota livros",
+"/autores": "Entrei na rota autores"
+}; */
+// Também não precisamos mais deste servidor...
+/* const server = http.createServer(
+(req, res) => {
+res.writeHead(200, {"Content-Type": "text/plain" } );
+res.end( rotas[req.url] );
+}
+); */
+// Aqui era server.listen...
+// Vira app.listen...
+app.listen( PORT,
     () => {
         console.log("Servidor ativo e aguardando requisições...");
     }
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+} );
+export default app;
